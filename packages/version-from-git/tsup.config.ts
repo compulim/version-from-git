130f9fd5b11adfc796055c7f1af770b92b5b62b2
@compulim <@compulim>
2025-12-24 02:43:44 +0000

    Bump scaffold

--- packages/version-from-git/tsup.config.ts
+++ packages/version-from-git/tsup.config.ts
@@ -6,7 +6,8 @@ export default defineConfig([
     entry: {
       'version-from-git': './src/index.ts'
     },
-    format: 'esm',
-    sourcemap: true
+    format: ['cjs', 'esm'],
+    sourcemap: true,
+    target: 'esnext'
   }
 ]);
